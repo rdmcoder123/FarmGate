96c08641c52795d81277d5fc0842bf4600d6f460
rdmcoder123 <dhatshinaklu@gmail.com>
2024-11-14 20:51:08 +0530

    initialize project structure with essential files and configurations

---
+++ client/src/components/Prices.js
@@ -0,0 +1,32 @@
+export const Prices = [
+  {
+    _id: 0,
+    name: "Rs0 to Rs19",
+    array: [0, 19],
+  },
+  {
+    _id: 1,
+    name: "Rs20 to Rs39",
+    array: [20, 39],
+  },
+  {
+    _id: 2,
+    name: "Rs40 to Rs59",
+    array: [40, 59],
+  },
+  {
+    _id: 3,
+    name: "Rs60 to Rs79",
+    array: [60, 79],
+  },
+  {
+    _id: 4,
+    name: "Rs80 to Rs99",
+    array: [80, 99],
+  },
+  {
+    _id: 4,
+    name: "Rs100 or more",
+    array: [100, 9999],
+  },
+];
